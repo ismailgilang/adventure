c245afbde26d966a579295376fbb79b7c1b52ba1
Ismail Gilang Maulana <ismailgilangm@gmail.com>
2026-05-19 18:25:32 +0700

    feat: configure cloudflare for admin app

---
+++ apps/admin/open-next.config.ts
@@ -0,0 +1,6 @@
+// default open-next.config.ts file created by @opennextjs/cloudflare
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig({
+	// For best results consider enabling R2 caching
+});
